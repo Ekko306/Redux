67c9dcadf953d91838525cba59a5701ef10634d3
Ekko306 <1186179435@qq.com>
2021-02-03 18:40:31 +0800

    add: mockjs

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = [
     {id: '0', name: 'Tianna Jenkins'},
@@ -13,3 +14,8 @@ const usersSlice = createSlice({
 })
 
 export default usersSlice.reducer
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await client.get('/fakeApi/users')
+    return response.users
+})
